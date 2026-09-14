b6e549b8bfb8bfa400c39dd00618852105276682
lyud333 <lyud333@gmail.com>
2026-09-14 19:39:32 +0900

    review

--- src/problem/Round2.java
+++ src/problem/Round2.java
@@ -3,13 +3,15 @@ package problem;
 public class Round2 {
     public static void main(String[] args) {
         int score = 86;
+        String grade;
         System.out.println("점수: " + score);
-        switch (score / 10) {
-            case 10: case 9: System.out.println("등급: A"); break;
-            case 8:          System.out.println("등급: B"); break;
-            case 7:          System.out.println("등급: C"); break;
-            default:         System.out.println("등급: D");
+        switch (score / 10){
+            case 10: case 9: grade = "A"; break;
+            case 8: grade = "B"; break;
+            case 7: grade = "C"; break;
+            default: grade = "D";
         }
+        System.out.println("등급: "+grade);
 
     }
 }
