897055a84b1e512856f3e7102220a2ae772245da
lyud333 <lyud333@gmail.com>
2026-09-14 20:49:17 +0900

    study5~8

--- src/problem/Round6.java
+++ src/problem/Round6.java
@@ -5,11 +5,14 @@ public class Round6 {
         int[] numbers = {12, 7, 18, 57, 24, 39, 16, 8};
         int sum = 0;
         System.out.print("홀수 번째 요소: ");
-        for (int i = 0; i < numbers.length; i += 2) {
-            System.out.print(numbers[i] + " ");
-            sum += numbers[i];
+        for(int i=0;i<numbers.length;i++){
+            if (i % 2 ==0){
+                System.out.print(numbers[i]+" ");
+                sum+=numbers[i];
+            }
+
         }
         System.out.println();
-        System.out.println("홀수 번째 요소의 합: " + sum);
+        System.out.println("홀수 번째 요소의 합: "+sum);
     }
 }
--- src/problem/Round8.java
+++ src/problem/Round8.java
@@ -3,8 +3,8 @@ package problem;
 public class Round8 {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
-            if (i % 2 == 0) for (int j = i * 5 + 1; j <= i * 5 + 5; j++) System.out.print(j + " ");
-            else            for (int j = i * 5 + 5; j >= i * 5 + 1; j--) System.out.print(j + " ");
+            if (i % 2 == 0) for (int j = i * 5 + 1;j <= i * 5 + 5; j++) System.out.print(j+" ");
+            else for (int j = i * 5 + 5;j>= i * 5 + 1; j--) System.out.print(j+" ");
             System.out.println();
         }
     }
